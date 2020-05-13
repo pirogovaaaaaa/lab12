d43d1a67b00b406cd992beae7dd1e214d45e872c
pirogovaaaaaa <pirogova.1001@mail.ru>
2020-05-13 23:22:21 +0300

    LABA 12

---
+++ src/worker.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<p>Имя: {{name}}</p>
+		<p>Фамилия: {{surname}}</p>
+		<p>Должность: {{type}}</p>
+	</div>
+</template>
+
+<script>
+export default{
+	data(){
+		return{
+			name: "Иван",
+			surname: "Иванов",
+			type: "Уборщик"
+		}
+	}
+}
+</script>
